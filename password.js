c11b24c905395beb559c6949077e339524ba7e3d
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-12-11 15:25:35 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,18 @@
-// code solution here
+const length = process.argv[2]
+const numberOfPasswords = process.argv[3]
+
+function generatePassword(length) {
+  let password = []
+
+  for (let i = 0; i < length; i++) {
+    password.push(Math.floor(Math.random() * 10)).toString()
+  }
+
+  console.log(password.join(''))
+}
+
+for (let i = 0; i < numberOfPasswords; i++) {
+  generatePassword(length)
+}
+
+process.exit(0)
